7bc9ea72302142af777164437ef0366b0d6ce816
dmc25 <dcoln@outlook.com>
2018-06-16 23:57:47 -0700

    Reboot Check Section

--- sophos script.ps1
+++ sophos script.ps1
@@ -1,3 +1,9 @@
+#############################################################
+#Sophos installation Script
+#Written By: Damian Coln
+#Modified: 6/16/2018
+#############################################################
+
 $deviceEncrption                    = "Yes"
 $appplicationBasedFileEncryption    = "No" 
 $localBasedFileEncryption           = "No"
@@ -5,8 +11,7 @@ $dataExchange                       = "No"
 $fileShare                          = "No"
 $cloudStorage                       = "No"
 $windows7_BitLocker                 = "No"
-$windows7_ProDefault2SGN            = "No"
-
+$windows7_ProDefault2SGN            = "No" 
 
 #Create scritpting log file
 #Variables for naming the file and locatoin
@@ -20,10 +25,9 @@ function LogWrite ([string] $logString) {
     Add-Content $strLogFileLocation -Value $logString
 }
 
-#########################################################################################
-
+##########################
 #Inital Log file creation
-
+##########################
 LogWrite -logString "SGN Modules selected for install"
 LogWrite -logString "Device Encryption: $deviceEncrption" 
 LogWrite -logString "ApplicatonBasedFileENcryption: $appplicationBasedFileEncryption"
@@ -34,5 +38,16 @@ LogWrite -logString "cloudStorage: $cloudStorage"
 LogWrite -logString "windows7_bitlocker: $windows7_BitLocker"
 LogWrite -logString "windows7_ProDefault2SGN: $windows7_ProDefault2SGN"
 
-#########################################################################################
+###############################################
+#Checks to see if a pending reboot is required.
+###############################################
+$isTrue = [string]::IsNullOrEmpty((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager' -Name PendingFileRenameOperations))
 
+if($isTrue){LogWrite -logString "A pending reboot was not detected, installation will continue"}
+
+else {
+    Get-Item -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager' | Remove-ItemProperty -Name PendingFileRenameOperations
+    LogWrite -logString "A pending reboot was deleted, installation will continue"
+}
+
+#########################################################################################
